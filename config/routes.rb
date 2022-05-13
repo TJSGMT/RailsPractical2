940b7380749d31da17bddbbcff3c99944344801a
tjs <tejas.g@simformsolutions.com>
2022-05-13 14:07:24 +0530

    Change in Rails-Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,9 @@
 Rails.application.routes.draw do
   get 'homes/index'
- 
-
   root "products#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # resources :products
-  # resources :orders
-
-  resources :products do
+   resources :products do
     resources :orders 
   end
-
   namespace :business do
     resources :customers, only: [:index, :edit, :create, :new, :update ] do
       member do
