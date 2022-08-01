6c4a1571cdfd7690ffe32d2638ff90aecfee3a8d
tjs <tejas.g@simformsolutions.com>
2022-08-01 12:28:02 +0530

    add product id to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
   has_many :orders,  dependent: :destroy
+
+
+  after_create do 
+    product = Stripe::Product.create(name: name)
+    price = Stripe::Price.create(product: product,unit_amount: self.price, currency: "usd")
+    update(stripe_product_id: product.id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_29_034431) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_29_110020) do
   create_table "addresses", force: :cascade do |t|
     t.string "house_name"
     t.string "street_name"
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_29_034431) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stripe_product_id"
   end
 
   create_table "users", force: :cascade do |t|
