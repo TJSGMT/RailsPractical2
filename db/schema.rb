930d5237fc9e9820ad41988639495f15b62f08a5
tjs <tejas.g@simformsolutions.com>
2022-08-31 16:05:38 +0530

    dauhh

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_20_103622) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_01_082547) do
   create_table "addresses", force: :cascade do |t|
     t.string "house_name"
     t.string "street_name"
@@ -64,6 +64,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_20_103622) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stripe_product_id"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -75,10 +78,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_20_103622) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stripe_customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "orders", "customers"
   add_foreign_key "orders", "products"
+  add_foreign_key "products", "users"
 end
